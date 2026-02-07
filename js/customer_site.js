ffcfdb8e13f5bfcfef8efb0c2b038693085def7b
JerryLee <lijiajun232@mails.ucas.ac.cn>
2026-02-08 01:21:54 +0800

    Update Maotai API endpoint and remove Qiqi resource

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,11 +1,7 @@
 const CUSTOMER_SITES = {
     maotai: {
-        api: 'https://caiji.maotaizy.cc/api.php/provide/vod',
+        api: 'https://caiji.maotaizy.cc/api.php/provide/vod/at/josn',
         name: '茅台资源',
-    },
-    qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
     }
 };
 
